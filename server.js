5f17354fda804a8d8c3d090e9de9574fa85a0328
melee-master <89125019+melee-master@users.noreply.github.com>
2022-07-04 11:41:25 +0530

    inputtin file & creating new entry in the db

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const multer = require("multer")
+
+const mongoose = require("mongoose")
+
+const dotenv= require('dotenv');
+
+dotenv.config();
+
+
+const upload = multer({dest: "uploads"})
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const bcrypt = require("bcrypt");
+
+const File = require('./models/file');
+
+app.set("view engine", "ejs");
+
+mongoose.connect(process.env.DATABASE_URL)
+
+app.get('/', (req, res)=> {
+    res.render("index")
+})
+app.post('/upload', upload.single("file"), async (req, res)=>{
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname,
+       
+    }
+    if(req.body.password != null && req.body.password !== "" ){
+        fileData.password = await bcrypt.hash(req.body.password, 10);
+    }
+    const file = await File.create(fileData)
+    
+})
+
+app.listen(PORT, (req, res)=> {
+    console.log(`Server started on port no. ${PORT}`);
+})
